f8c9c010299986677f473be1ce347528d1a253c2
Michael Fessenden <MikeFez@gmail.com>
2023-06-16 21:00:55 -0400

    debug meural_api.playlist_ids_by_name

--- app/main.py
+++ app/main.py
@@ -133,6 +133,7 @@ def _subtask_add_orphaned_images_to_remove_from_icloud_album(icloud_album_obj, m
 
     if orphaned_icloud_images:
         if not Env.DRY_RUN:
+            logger.error(meural_api.playlist_ids_by_name)
             if Env.DELETE_FROM_ICLOUD_PLAYLIST_NAME not in meural_api.playlist_ids_by_name.keys():
                 logger.info(f"\t'{Env.DELETE_FROM_ICLOUD_PLAYLIST_NAME}' playlist not found in Meural - creating it")
                 meural_api.create_playlist(
